4c924aa6874c615d6c01b9b64a17b1990adac9a5
Diego Ramos <diegoal_2098@hotmail.com>
2019-10-30 10:40:32 -0500

    Add Spanish translation

--- lib/backend/models/alert/Translations.dart
+++ lib/backend/models/alert/Translations.dart
@@ -5,10 +5,12 @@ import 'package:thepublictransport_app/backend/models/alert/Translated.dart';
 class Translations {
   Translated en;
   Translated de;
+  Translated es;
 
   Translations({
     this.en,
     this.de,
+    this.es
   });
 
   factory Translations.fromRawJson(String str) => Translations.fromJson(json.decode(str));
@@ -18,10 +20,12 @@ class Translations {
   factory Translations.fromJson(Map<String, dynamic> json) => Translations(
     en: json["en"] == null ? null : Translated.fromJson(json["en"]),
     de: json["de"] == null ? null : Translated.fromJson(json["de"]),
+    es: json["es"] == null ? null : Translated.fromJson(json["es"])
   );
 
   Map<String, dynamic> toJson() => {
     "en": en == null ? null : en.toJson(),
     "de": de == null ? null : de.toJson(),
+    "es": es == null ? null : es.toJson()
   };
 }
