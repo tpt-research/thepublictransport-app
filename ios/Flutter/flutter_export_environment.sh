7beb91f77197b47122ffcc37635b396dc8b3be20
Eddy <eddyhu71@gmail.com>
2019-11-02 11:10:36 +0700

    Update flutter_export_environment.sh

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/pdesire/.devfiles/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/pdesire/Workspace/Projects/thepublictransport-app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=D:\Flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter Project\thepublictransport-app"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/pdesire/.devfiles/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=2.5.0"
-export "FLUTTER_BUILD_NUMBER=25"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=2.6.0"
+export "FLUTTER_BUILD_NUMBER=26"
